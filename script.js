099e82eec4d5f8e7ffd94f34489225f7ff344f13
Anirudh <60171262+Blank333@users.noreply.github.com>
2024-04-23 15:10:26 +0530

    Created button selectors Added vector images for buttons

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-console.log('====================================');
-console.log("Connected");
-console.log('====================================');
+// console.log('====================================');
+// console.log("Connected");
+// console.log('====================================');
+
+// fetch("https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json")
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
