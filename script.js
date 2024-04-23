b8bcb2ce475b7c811f981718bdc343987cda5a04
Anirudh <60171262+Blank333@users.noreply.github.com>
2024-04-24 01:31:32 +0530

    Implemented better button styling Cleaned up css file Added comments

--- script.js
+++ script.js
@@ -4,30 +4,41 @@ fetch("https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json"
     return res.json();
   })
   .then((data) => {
-    console.log(data);
     const items = data.categories;
-    const container = document.querySelector(".items");
+    //Select the html template element and other required elements
     const template = document.getElementById("item-template");
+    const container = document.querySelector(".items");
+    const categories = document.querySelectorAll(".category");
 
-    const cateogires = document.querySelectorAll(".category");
-
+    //Render the mens section by default
     renderItems(items[0].category_products);
 
-    cateogires.forEach((button) => {
+    //Add a click event on all category buttons
+    categories.forEach((button) => {
       button.addEventListener("click", () => {
+        //Remove the styling for the selected button and add it to the clicked button
+        categories.forEach((button) => {
+          button.classList.remove("selected");
+        });
         button.classList.add("selected");
+
+        //Select the category of the button and get the products for the appropriate category for rendering
         const category = button.classList[1];
         const categoryProducts = items.find((item) => item.category_name.toLowerCase() === category).category_products;
         renderItems(categoryProducts);
       });
     });
 
+    //Render function
     function renderItems(products) {
+      //Clear the cards from the html
       container.innerHTML = "";
 
       products.forEach((item) => {
+        //Clone the template for rendering each product
         const card = template.content.cloneNode(true);
 
+        //Get the information
         const title = card.querySelector(".title");
         title.textContent = item.title.length > 10 ? item.title.slice(0, 10) + "..." : item.title;
 
@@ -36,6 +47,7 @@ fetch("https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json"
         image.alt = item.title;
 
         const badge = card.querySelector(".badge");
+        //Do not render an empty badge
         if (!item.badge_text) badge.style.display = "none";
         else badge.textContent = item.badge_text;
 
@@ -46,6 +58,7 @@ fetch("https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json"
         old_price.textContent = `Rs. ${item.compare_at_price}.00`;
 
         const discount = card.querySelector(".discount");
+        //Calculating the discount to the nearest integer
         const discount_calc = Math.round(
           ((parseInt(item.compare_at_price) - parseInt(item.price)) / parseInt(item.compare_at_price)) * 100
         );
@@ -57,4 +70,7 @@ fetch("https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json"
         container.appendChild(card);
       });
     }
+  })
+  .catch((err) => {
+    console.log(`Error fetching from API ${err}`);
   });
